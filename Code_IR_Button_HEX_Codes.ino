7c0b2290b63bead98e5b55c089e92196ea234849
Vignesh D <159744719+Vignesh830@users.noreply.github.com>
2024-09-18 14:59:44 +0530

    Update Code_IR_Button_HEX_Codes.ino

--- Code_IR_Button_HEX_Codes.ino
+++ Code_IR_Button_HEX_Codes.ino
@@ -1,14 +1,3 @@
- /**********************************************************************************
- *  TITLE: Get the Hex code from any IR remote.
- *  Click on the following links to learn more. 
- *  YouTube Video: https://youtu.be/dtHGJcNkaMo
- *  Related Blog : https://iotcircuithub.com/arduino-projects/
- *  by Tech StudyCell
- * 
- *  Download the Libraries:
- *  IRremote Library 3.3.0: https://github.com/Arduino-IRremote/Arduino-IRremote
- **********************************************************************************/
-
 #include <IRremote.h>
  
 int IR_RECV_PIN = A0;  //Update the pin as per circuit
